a773cb20c73ad3b1ee04d1e269fb8d2628be2a7a
Victor Chang <v.chang.yu@gmail.com>
2018-09-29 08:48:41 -0300

    replace GCD with performingBackgroundTask

--- CoreDataProject/Companies/CompaniesController.swift
+++ CoreDataProject/Companies/CompaniesController.swift
@@ -39,6 +39,23 @@ class CompaniesController: UITableViewController {
 	
 	@objc fileprivate func doWork() {
 		print("Trying to do work")
+	
+		CoreDataManager.shared.persistentContainer.performBackgroundTask { (backgroundContext) in
+			
+			(0...20000).forEach { (value) in
+				print(value)
+				let company = Company(context: backgroundContext)
+				company.name = String(value)
+			}
+			
+			do {
+				try backgroundContext.save()
+			} catch let error {
+				print("Failed to save: ", error)
+			}
+			
+		}
+		
 	}
 	
 	@objc fileprivate func handleReset() {
